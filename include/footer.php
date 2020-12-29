38098b20f601f0963819943a22eeb503c35277cc
BayazidT <btnirob13@gmail.com>
2020-12-29 22:21:54 +0600

    First commit

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+
+        <h3>&copy EShop.com | All right received!</h3>
+
+        </div>
+       
+    </body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            JS Intro
+        </title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body id="demo">
+        <div class="header">
+            <div class="row">
+                <div class="col-4">
+                    <h1>EShop</h1>
+    
+                </div>
+                <div class="col-4">
+                   Search Form
+                </div>
+                
+                
+                <div class="col-4">
+                    NO worry about that things  Nothing
+                </div>
+            </div>
+           
+            <nav class="nav">
+                <ul>
+                    <li>EShop</li>
+                    <li>Offers</li>
+                    <li>Cart</li>
+                    <li>Register</li>
+                    <li>Login</li>
+                    <li>Blog</li>
+                </ul>
+            </nav>
+
+        </div>
